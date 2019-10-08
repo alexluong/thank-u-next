b8e0a7775b47d56276b09d24b671c92c16310188
Alex Luong <alex@alexluong.com>
2019-10-07 23:45:15 -0500

    feat(thank-u-next): init thank-u-next

---
+++ thank-u-next/index.js
@@ -0,0 +1,51 @@
+const badWordsArr = require("badwords/array")
+
+const hello = ["hello", "helo", "helllo", "helloo", "hellooo", "hola"]
+const hi = ["hi", "hii", "hiii", "hiiii", "hhi", "hey", "yo"]
+const ws = ["how", "what"]
+const pronoun = ["hun", "honey", "sweetie", "baby", "bae", "babe"]
+const time = ["today", "morning", "night", "day", "date"]
+const developer = ["developer", "coder", "programmer"]
+const beautiful = ["beautiful", "pretty"]
+
+const badWords = [
+  ...badWordsArr,
+  ...hello,
+  ...hi,
+  ...ws,
+  ...pronoun,
+  ...time,
+  ...developer,
+  ...beautiful,
+  "good",
+  "doing",
+  "you",
+  "there",
+  "nice",
+  "meet",
+  "a",
+]
+
+const trim = word => word.replace(/[^\w]/g, "")
+const toLowerCase = word => word.toLowerCase()
+const isBadWord = word => badWords.includes(word)
+
+/**
+ *
+ * @param {string} message
+ */
+function isNext(message) {
+  const tokens = message
+    .split(" ")
+    .filter(word => word.match(/[a-z]/i))
+    .map(trim)
+    .map(toLowerCase)
+    .map(isBadWord)
+
+  const numTrue = tokens.reduce((num, isTrue) => (isTrue ? num + 1 : num))
+  const percent = (numTrue * 100) / tokens.length
+
+  return percent >= 50
+}
+
+exports.isNext = isNext
