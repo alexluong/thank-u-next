5feb85f6204fbd781383034d75ee589fa7e74882
Alex Luong <alex@alexluong.com>
2019-10-09 00:48:45 -0500

    feat(client): add more info for Webhook card in dashboard

--- client/src/components/Webhook.js
+++ client/src/components/Webhook.js
@@ -1,6 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { Heading, FormLabel, Switch } from "@chakra-ui/core"
+import { Heading, FormLabel, Switch, Text, Code, Alert } from "@chakra-ui/core"
 import Card from "./Card"
 import { COLORS } from "../utils/constants"
 
@@ -8,9 +8,9 @@ function Webhook({ isWebhookEnabled, toggleWebhook }) {
   return (
     <Card>
       <Heading as="h2" size="md" mb={4}>
-        Filter
+        Twitter Filter
       </Heading>
-      <FormLabel htmlFor="webhook">Enable Filter</FormLabel>
+      <FormLabel htmlFor="webhook">Enable Filter?</FormLabel>
       <Switch
         id="webhook"
         color={COLORS.primary}
@@ -18,6 +18,17 @@ function Webhook({ isWebhookEnabled, toggleWebhook }) {
         isChecked={isWebhookEnabled}
         onChange={() => toggleWebhook()}
       />
+      <Text mt={4}>
+        You can choose to turn on and off <Code>thank u, next</Code> whenever
+        you want using this switch.
+      </Text>
+
+      {!isWebhookEnabled && (
+        <Alert borderRadius={6} mt={8} bg="pink.100">
+          Your Twitter messages are not filtered at the moment. Please turn the
+          switch on if you want to use the filter.
+        </Alert>
+      )}
     </Card>
   )
 }
